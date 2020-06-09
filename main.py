b383816db90365f6d954ec77891e5ee8f8ea1af6
Mrestof <naruto.golubev@gmail.com>
2020-06-09 14:15:38 +0300

    Add some tests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import unittest
 from enum import Enum
 
 from fastapi import FastAPI
@@ -39,3 +40,18 @@ def calc(op: Operation, a: float, b: float = 1):
 @app.get('/compute')
 def compute(equation: str):
     return {'result': eval(equation)}
+
+
+# test zone
+class TestApp(unittest.TestCase):
+
+    def test_calc(self):
+        self.assertEqual(calc(Operation.add, 8, 2)['result'], 10, 'Should equal to 10')
+        self.assertEqual(calc(Operation.sub, 8, 2)['result'], 6, 'Should equal to 6')
+        self.assertEqual(calc(Operation.div, 8, 2)['result'], 4, 'Should equal to 4')
+        self.assertEqual(calc(Operation.mul, 8, 2)['result'], 16, 'Should equal to 16')
+        self.assertEqual(calc(Operation.exp, 8, 2)['result'], 64, 'Should equal to 64')
+        self.assertEqual(calc(Operation.sqr, 9)['result'], 3, 'Should equal to 3')
+
+    def test_compute(self):
+        self.assertEqual(compute(' 3 *5+ 40 /2-5** 2')['result'], 10, 'Should equal to 10')
