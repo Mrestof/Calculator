5041e7296dec2b234eb170a0a3301ebdf74fe233
Mrestof <naruto.golubev@gmail.com>
2020-06-09 17:00:17 +0300

    Add MongoDB support. Add one little exception to prevent ServerInternalError.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import unittest
 from enum import Enum
 
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 
 class Operation(str, Enum):
@@ -16,6 +17,13 @@ class Operation(str, Enum):
 
 app = FastAPI()
 
+client = MongoClient(
+    'mongodb+srv://calco:calco@calculatoroperations-7lpdk.mongodb.net/CalculatorOperations?retryWrites=true&w=majority'
+)
+db = client.CalculatorOperations
+atomic_ops = db.atomic_operations
+long_ops = db.long_operations
+
 
 @app.get('/')
 def home():
@@ -23,35 +31,53 @@ def home():
 
 
 @app.get('/calc/{op}')
-def calc(op: Operation, a: float, b: float = 1):
+def calc(op: Operation, a: float, b: float = 1, send_to_db: bool = True):
+    output = {'operation': op.value, 'operands': [a, b]}
+
     if op == op.add:
-        return {'result': a + b}
+        output['result'] = a + b
     if op == op.sub:
-        return {'result': a - b}
+        output['result'] = a - b
     if op == op.div:
-        return {'result': a / b}
+        output['result'] = a / b
     if op == op.mul:
-        return {'result': a * b}
+        output['result'] = a * b
     if op == op.exp:
-        return {'result': a ** b}
-    return {'result': math.sqrt(a)}
+        output['result'] = a ** b
+    if op == op.sqr:
+        output['result'] = math.sqrt(a)
+
+    if send_to_db:
+        atomic_ops.insert_one(output)
+        output.pop('_id')
+
+    return output
 
 
 @app.get('/compute')
-def compute(equation: str):
-    return {'result': eval(equation)}
+def compute(equation: str, send_to_db: bool = True):
+    try:
+        output = {'equation': equation.replace(' ', ''), 'result': eval(equation)}
+    except SyntaxError:
+        return {'SyntaxError': 'Check the equation for some typos'}
+
+    if send_to_db:
+        long_ops.insert_one(output)
+        output.pop('_id')
+
+    return output
 
 
 # test zone
 class TestApp(unittest.TestCase):
 
     def test_calc(self):
-        self.assertEqual(calc(Operation.add, 8, 2)['result'], 10, 'Should equal to 10')
-        self.assertEqual(calc(Operation.sub, 8, 2)['result'], 6, 'Should equal to 6')
-        self.assertEqual(calc(Operation.div, 8, 2)['result'], 4, 'Should equal to 4')
-        self.assertEqual(calc(Operation.mul, 8, 2)['result'], 16, 'Should equal to 16')
-        self.assertEqual(calc(Operation.exp, 8, 2)['result'], 64, 'Should equal to 64')
-        self.assertEqual(calc(Operation.sqr, 9)['result'], 3, 'Should equal to 3')
+        self.assertEqual(calc(Operation.add, 8, 2, False)['result'], 10, 'Should equal to 10')
+        self.assertEqual(calc(Operation.sub, 8, 2, False)['result'], 6, 'Should equal to 6')
+        self.assertEqual(calc(Operation.div, 8, 2, False)['result'], 4, 'Should equal to 4')
+        self.assertEqual(calc(Operation.mul, 8, 2, False)['result'], 16, 'Should equal to 16')
+        self.assertEqual(calc(Operation.exp, 8, 2, False)['result'], 64, 'Should equal to 64')
+        self.assertEqual(calc(Operation.sqr, 9, 1, False)['result'], 3, 'Should equal to 3')
 
     def test_compute(self):
-        self.assertEqual(compute(' 3 *5+ 40 /2-5** 2')['result'], 10, 'Should equal to 10')
+        self.assertEqual(compute(' 3 *5+ 40 /2-5** 2', False)['result'], 10, 'Should equal to 10')
