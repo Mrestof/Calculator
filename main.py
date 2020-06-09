6cddf846e71af3233e5cf6ef6c01733fec3362c3
Mrestof <naruto.golubev@gmail.com>
2020-06-09 13:51:53 +0300

    Replace couple of different operation functions to one that does all operations. Add new way of calculating things.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import math
+from enum import Enum
 
 from fastapi import FastAPI
+
+
+class Operation(str, Enum):
+    add = 'add'
+    sub = 'sub'
+    div = 'div'
+    mul = 'mul'
+    exp = 'exp'
+    sqr = 'sqr'
+
 
 app = FastAPI()
 
@@ -10,31 +21,21 @@ def home():
     return {'api_name': 'Calculator', 'page': 'home'}
 
 
-@app.get('/add')
-def add(a: float, b: float):
-    return {'result': a + b}
-
-
-@app.get('/sub')
-def sub(a: float, b: float):
-    return {'result': a - b}
-
-
-@app.get('/div')
-def div(a: float, b: float):
-    return {'result': a / b}
-
-
-@app.get('/mul')
-def mul(a: float, b: float):
-    return {'result': a * b}
-
-
-@app.get('/exp')
-def exp(a: float, b: float):
-    return {'result': a ** b}
-
-
-@app.get('/sqr')
-def sqr(a: float):
+@app.get('/calc/{op}')
+def calc(op: Operation, a: float, b: float = 1):
+    if op == op.add:
+        return {'result': a + b}
+    if op == op.sub:
+        return {'result': a - b}
+    if op == op.div:
+        return {'result': a / b}
+    if op == op.mul:
+        return {'result': a * b}
+    if op == op.exp:
+        return {'result': a ** b}
     return {'result': math.sqrt(a)}
+
+
+@app.get('/compute')
+def compute(equation: str):
+    return {'result': eval(equation)}
